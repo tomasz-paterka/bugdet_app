58de64e791f092dff46f93a6ebd643b3ecddd5d2
Tomasz Paterka <tomekpaterka@gmail.com>
2019-11-01 14:04:34 +0100

    Add Expense, Income function constructors, addItem function for creating new items

--- js/app.js
+++ js/app.js
@@ -1,5 +1,57 @@
 var budgetController = (function() {
 
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on type 'inc' or 'exp'
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into data structure
+      data.allItems[type].push(newItem);
+
+      // Return new item
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
 
 })();
 
@@ -17,7 +69,7 @@ var UIController = (function() {
     getInput: function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
-        decription: document.querySelector(DOMstrings.inputDescription).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       }
     },
@@ -39,7 +91,6 @@ var controller = (function(budgetCtrl, UICtrl) {
     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(e) {
-     
       if (e.keyCode === 13 || e.which === 13) {
         ctrlAddItem();
       } 
@@ -47,13 +98,14 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
-    var input = UICtrl.getInput();
-    console.log(input);
+    var input, newItem;
+
+    input = UICtrl.getInput();
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
   };
 
   return {
     init: function() {
-      console.log('start');
       setupEventListeners();
     }
   };
